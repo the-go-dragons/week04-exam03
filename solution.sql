5f018edd5d048f3390d905c03b8f9f00ebab1ad7
Muhammad Fazel Samarghandi <79993775+fazelsamar@users.noreply.github.com>
2023-05-16 13:33:32 +0330

    Complete section 1 and 2 and 3.

--- solution.sql
+++ solution.sql
@@ -1,8 +1,22 @@
 -- Section1
-    your 1st query here
+SELECT name, phone FROM customers
+JOIN orders on customers.id = orders.customer_id
+GROUP BY orders.customer_id
+ORDER BY COUNT(orders.customer_id) DESC
+LIMIT 1;
 -- Section2
-    your 2nd query here
+select restaurant_foods.food_id as id, foods.name as name from orders
+JOIN restaurant_foods on orders.restaurant_food_id = restaurant_foods.id
+JOIN foods on restaurant_foods.food_id = foods.id
+group by restaurant_foods.food_id
+order by avg(orders.rate) DESC, restaurant_foods.food_id
+LIMIT 10;
 -- Section3
-    your 3rd query here
+select restaurant_foods.restaurant_id as id, restaurants.name as name from orders
+JOIN restaurant_foods on orders.restaurant_food_id = restaurant_foods.id
+JOIN restaurants on restaurant_foods.restaurant_id = restaurants.id
+group by restaurant_foods.restaurant_id
+order by avg(orders.rate) DESC, restaurant_foods.restaurant_id
+LIMIT 10;
 -- Section4
-    your 4th query here
+-- TODO
