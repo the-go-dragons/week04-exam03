3e98c24582cff5add8c7d446da4ef5ea10abe7b2
Mohammed Behjoo <48823293+mohammedbehjoo@users.noreply.github.com>
2023-05-17 12:00:45 +0330

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,12 +1,19 @@
 -- Section1
-    select c.name, c.phone
-    from customers c
-    join orders o on c.id = o.customer_id
-    group by c.id,c.name, c.phone
-    order by count(*) desc
-    limit 1;
+select c.name, c.phone
+from customers c
+join orders o on c.id = o.customer_id
+group by c.id,c.name, c.phone
+order by count(*) desc
+limit 1;
+
 -- Section2
-    your 2nd query here
+select f.id,f.name
+from foods f
+join restaurant_foods rf on f.id = rf.food_id
+join orders o on rf.id = o.restaurant_food_id
+group by f.id,f.name
+order by avg(rate) desc,f.id
+limit 10;
 -- Section3
     your 3rd query here
 -- Section4
