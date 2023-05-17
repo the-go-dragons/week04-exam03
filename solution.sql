28e30bcec92c5d4222548cba79d20253ee1f0ed6
Mohammed Behjoo <48823293+mohammedbehjoo@users.noreply.github.com>
2023-05-17 11:22:02 +0330

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,5 +1,10 @@
 -- Section1
-    your 1st query here
+    select c.name, c.phone
+    from customers c
+    join orders o on c.id = o.customer_id
+    group by c.id,c.name, c.phone
+    order by count(*) desc
+    limit 1;
 -- Section2
     your 2nd query here
 -- Section3
