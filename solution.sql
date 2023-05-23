3229613ac5ec509a2b52207730c214984515cf1b
Ardeshir <e-job@protonmail.com>
2023-05-23 15:10:15 +0330

    Update solution.sql, Apply clean code format
    Apply clean code format
    
    Please accept all changes and rewrite with no mercy!

--- solution.sql
+++ solution.sql
@@ -1,29 +1,66 @@
 -- Section1
-select c.name,c.phone from customers c
-    inner  join orders o on c.id = o.customer_id
-     inner join restaurant_foods rf on rf.id = o.restaurant_food_id
-                    group by c.name, c.phone
-order by count(o.id) desc, count(rf.restaurant_id)  desc
- limit 1;
+SELECT
+  c.name,
+  c.phone
+FROM
+  customers c
+  INNER JOIN orders o ON c.id = o.customer_id
+  INNER JOIN restaurant_foods rf ON rf.id = o.restaurant_food_id
+GROUP BY
+  c.name,
+  c.phone
+ORDER BY
+  count(o.id) DESC,
+  count(rf.restaurant_id) DESC
+LIMIT
+  1;
 
 -- Section2
-    select f.id,f.name from  orders o
-     inner join restaurant_foods rf on rf.id = o.restaurant_food_id
-     inner join foods f on rf.food_id = f.id
-                    group by f.id,f.name
-order by avg(o.rate) desc, f.id
- limit 10;
+SELECT
+  f.id,
+  f.name
+FROM
+  orders o
+  INNER JOIN restaurant_foods rf ON rf.id = o.restaurant_food_id
+  INNER JOIN foods f ON rf.food_id = f.id
+GROUP BY
+  f.id,
+  f.name
+ORDER BY
+  avg(o.rate) DESC,
+  f.id
+LIMIT
+  10;
+
 -- Section3
-   select r.id,r.name from  orders o
-     inner join restaurant_foods rf on rf.id = o.restaurant_food_id
-     inner join restaurants r on rf.restaurant_id = r.id
-                    group by r.id,r.name
-order by avg(o.rate) desc, r.id
- limit 10; 
+SELECT
+  r.id,
+  r.name
+FROM
+  orders o
+  INNER JOIN restaurant_foods rf ON rf.id = o.restaurant_food_id
+  INNER JOIN restaurants r ON rf.restaurant_id = r.id
+GROUP BY
+  r.id,
+  r.name
+ORDER BY
+  avg(o.rate) DESC,
+  r.id
+LIMIT
+  10;
+
 -- Section4
-  select c.name,c.phone  from customers c
-    inner  join orders o on c.id = o.customer_id
-     inner join restaurant_foods rf on rf.id = o.restaurant_food_id
-            group by c.name,c.phone
-            having  COUNT(DISTINCT rf.restaurant_id)>=5
-order by c.name;
+SELECT
+  c.name,
+  c.phone
+FROM
+  customers c
+  INNER JOIN orders o ON c.id = o.customer_id
+  INNER JOIN restaurant_foods rf ON rf.id = o.restaurant_food_id
+GROUP BY
+  c.name,
+  c.phone
+HAVING
+  count(DISTINCT rf.restaurant_id) >= 5
+ORDER BY
+  c.name;
